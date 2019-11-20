a502b7ff053068e02fe6a19477795b1b10a78ab7
malin <348475822@qq.com>
2019-11-20 17:57:42 +0800

    day2

---
+++ nodejs/02 static/07request.js
@@ -0,0 +1,33 @@
+const request = require('request');
+
+const http = require('http');
+
+/*
+    request(fn) 用于发送ajax请求的模块，类似jq的$.ajax()
+        fn : 回调函数，里面有三个参数
+            * 参数一：err 错误
+            * 参数二：res 响应
+            * 参数三：data 响应的内容
+*/
+
+//到微博官网，查找数据接口，记得用network的 xhr模式
+// request('https://m.weibo.cn/api/config/list', (error, response, data) => {
+//     console.log(data);
+// });
+
+//把请求到的数据传给前端，前端用ajax请求即可。这就是服务器代理
+http.createServer((req, res) => {
+    /*
+        类似于jquery ajax请求
+    */
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*'
+    }); request
+    request('https://m.weibo.cn/api/config/list', (error, response, data) => {
+        res.end(data);
+    });
+}).listen(3006, () => {
+    console.log('server is running on http://localhost:3006')
+});
+
+
