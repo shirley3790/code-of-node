8dc0b7025789688d95debceb05a81b1fe350d3fd
shirley Lin <348475822@qq.com>
2019-11-22 23:56:30 +0800

    day3

--- nodejs/03spider/10express_proxy.js
+++ nodejs/03spider/10express_proxy.js
@@ -3,18 +3,23 @@ const express = require('express');
 
 let app = express();
 
-app.use(express.static('./'))
+app.use(express.static('./'));
 
-app.all('*', function(req, res, next) {
+//如果访问根目录，返回字符串“这是首页”给前端
+app.get('/', (req, res) => {
+    res.send('这是4008端口首页'); //类似php里面的echo
+});
+
+app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
 
     // 跨域请求CORS中的预请求
-    if(req.method=="OPTIONS") {
-      res.send(200);/*让options请求快速返回*/
-    } else{
-      next();
+    if (req.method == "OPTIONS") {
+        res.send(200); /*让options请求快速返回*/
+    } else {
+        next();
     }
 });
 
@@ -22,10 +27,10 @@ app.all('*', function(req, res, next) {
 //如果请求以/sinaapi开头，则进入代理模式
 // http://localhost:4008/sinaapi/api/config/list 转成 https://m.weibo.cn/sinaapi/api/config/list
 app.use('/sinaapi', proxy({
-    "target": "https://m.weibo.cn",
+    "target": "https://m.weibo.cn", //你要代理的网址
     "changeOrigin": true,
-    "pathRewrite": {
-        "^/sinaapi" : "/"
+    "pathRewrite": { //重新路径
+        "^/sinaapi": "/"
     }
 }));
 
@@ -33,10 +38,10 @@ app.use('/api', proxy({
     "target": "https://m.xx.cn",
     "changeOrigin": true,
     "pathRewrite": {
-        "^/api" : "/"
+        "^/api": "/"
     }
 }));
 
-app.listen(4008, function(){
-    console.log('Server running on http://localhost:4004');
+app.listen(4008, function () {
+    console.log('Server running on http://localhost:4008');
 });
