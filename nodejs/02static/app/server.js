ef554cae0ebc3304f3f1114e4dc0e53f2ee81641
shirley Lin <348475822@qq.com>
2019-11-26 22:30:17 +0800

    new

--- nodejs/02static/app/server.js
+++ nodejs/02static/app/server.js
@@ -3,39 +3,41 @@
  *  支持：css,html,js,image
  */
 
- const http = require('http');
- const fs = require('fs');
- const url = require('url');
- const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
 
 //  引入mime类型
 const mime = require('./mime');
 
 
 //  创建http服务器
-http.createServer((req,res)=>{
+http.createServer((req, res) => {
     let pathname = url.parse(req.url).pathname;
-
+    // console.log(pathname);
     // 获取绝对路径，用于读取图片内容
     let realPath;
 
-    res.writeHead(200,{'Content-Type':`text/plain;charset=utf-8`});
+    res.writeHead(200, {
+        'Content-Type': `text/plain;charset=utf-8`
+    });
     // 路由Router
-    switch(pathname){
+    switch (pathname) {
         case '/':
-            realPath =  path.join(__dirname,'index.html');
+            realPath = path.join(__dirname, 'index.html');
             break;
         case '/login':
-            realPath =  path.join(__dirname,'html/login.html');
+            realPath = path.join(__dirname, 'html/login.html');
             break;
         case '/reg':
-            realPath =  path.join(__dirname,'html/reg.html');
+            realPath = path.join(__dirname, 'html/reg.html');
             break;
         case '/usercenter':
-            realPath =  path.join(__dirname,'html/usercenter.html');
+            realPath = path.join(__dirname, 'html/usercenter.html');
             break;
         default:
-            realPath = path.join(__dirname,pathname);
+            realPath = path.join(__dirname, pathname);
     }
 
 
@@ -43,14 +45,20 @@ http.createServer((req,res)=>{
     let ext = path.extname(realPath).slice(1);
 
     // 根据路径读取静态资源文件内容
-    fs.readFile(realPath,(err,data)=>{
-        if(err){
-            res.writeHead(404,{'Content-Type':`text/plain;charset=utf-8`});
+    fs.readFile(realPath, (err, data) => {
+        if (err) {
+            res.writeHead(404, {
+                'Content-Type': `text/plain;charset=utf-8`
+            });
             res.end(`你访问你的url:${pathname} 不存在`);
         }
-        res.writeHead(200,{'Content-Type':`${mime[ext]};charset=utf-8`});
+        res.writeHead(200, {
+            'Content-Type': `${mime[ext]};charset=utf-8`
+        });
         res.end(data);
     });
-}).listen(3005,()=>{
+
+
+}).listen(3005, () => {
     console.log('server is running on port 3005')
 })
